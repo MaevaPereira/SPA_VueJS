15c37db6e16bd4665e4ed721ff85a65073768282
Maeva Pereira <maeva.pereira@le-campus-numerique.fr>
2026-04-30 14:53:27 +0200

    fin étape 3

---
+++ vue-project/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+
+const onListClick = () => {
+  alert("test")
+}
+</script>
+
+<template>
+
+<div>
+  <h1>Météo</h1>
+
+   <p @click= "onListClick">
+   lien météo... (en théorie) :  <a href="https://vuejs.org/" target="_blank" rel="noopener">titre du petit lien</a>
+   </p>
+</div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue-project/src/views/CitiesList.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="cities-list">
+  <h1>Météo - Liste des villes</h1>
+</div>
+</template>
+
+<style scoped>
+
+</style>
