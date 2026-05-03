292a497d2501e999c944491b5735fc4556a978ad
MaevaPereira <maeva.pereira@le-campus-numerique.fr>
2026-05-03 19:16:03 +0200

    Passage de props au composant City

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -6,7 +6,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: "/villes", component: CitiesList },
-    { path: "/ville", component: City }
+    //{ path: "/ville", component: City }
   ],
 })
 
